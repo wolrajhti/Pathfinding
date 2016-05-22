f00e96cd1a9cbf0310d4b43070e12948483da59c
wolrajhti <pierre.bouteloup@hotmail.fr>
2016-05-22 18:53:29 +0200

    wip

--- main.lua
+++ main.lua
@@ -1,17 +1,23 @@
 class = require'middleclass'
--- local cpml = require'cpml'
--- local Delaunay = require'delaunay'
--- local HC = require'HC'
--- local Polygon = require'HC.polygon'
 Vector = require'Geometry.modules.vector'
--- local Surface = require'Geometry.modules.surface_backup'
 local Surface = require'Geometry.modules.surface'
 
 love.graphics.setLineStyle('smooth')
 
-pts = {}
--- triangles = {}
 debug = io.open('debug.txt', 'w+')
+
+-- DEBUG = true
+
+surf = Surface(unpack({190, 482, 165, 237, 279, 126, 421, 71, 384, 94, 314, 186, 264, 232, 247, 284, 310, 344, 358, 362, 
+372, 291, 386, 223, 439, 152, 477, 107, 512, 73, 573, 94, 550, 108, 524, 132, 487, 156, 454, 206, 477, 239, 527, 228, 613, 
+217, 654, 190, 619, 128, 628, 104, 676, 81, 700, 128, 705, 198, 662, 251, 600, 266, 555, 269, 614, 321, 669, 383, 728, 534, 
+687, 565, 607, 524, 596, 425, 552, 362, 502, 315, 446, 284, 442, 321, 448, 365, 484, 410, 534, 529, 513, 577, 474, 557, 436, 
+495, 427, 423, 395, 397, 378, 418, 355, 452, 342, 516, 352, 544, 376, 535, 403, 522, 419, 540, 410, 561, 398, 572, 371, 567, 
+351, 566, 329, 574, 292, 573, 268, 575, 227, 574, 201, 566, 226, 550, 268, 551, 293, 554, 303, 526, 283, 504, 266, 517, 232, 
+522, 195, 533, 157, 542, 109, 546, 60, 442, 64, 381, 65, 267, 82, 264, 101, 326, 104, 407, 119, 466, 141, 495, 173, 514, 212, 
+508, 251, 481, 290, 470, 300, 455, 267, 435, 281, 406, 319, 409, 341, 390, 308, 380, 265, 369, 245, 388, 240, 411, 243, 451, 
+229, 474, 214, 491}))
+
 function love.load()
 end
 
@@ -21,73 +27,43 @@ end
 function love.draw()
 	if surf then
 		surf:draw()
-		local M = surf:locate(Vector(love.mouse.getPosition()))
-		if M then
-			love.graphics.setColor(255, 200, 200, 100)
-			love.graphics.polygon('fill', M[1].x, M[1].y, M[2].x, M[2].y, M[3].x, M[3].y)
-		end
+		-- local M = surf:locate(Vector(love.mouse.getPosition()))
+		-- if M then
+			-- love.graphics.setColor(255, 0, 0)
+			-- Vector(surf.vertices[M[1]].x, surf.vertices[M[1]].y):draw()
+			-- love.graphics.setColor(0, 255, 0)
+			-- Vector(surf.vertices[M[2]].x, surf.vertices[M[2]].y):draw()
+			-- love.graphics.setColor(0, 0, 255)
+			-- Vector(surf.vertices[M[3]].x, surf.vertices[M[3]].y):draw()
+
+			-- for i = 4, 6 do
+				-- if i == 4 then love.graphics.setColor(255, 0, 0) end
+				-- if i == 5 then love.graphics.setColor(0, 255, 0) end
+				-- if i == 6 then love.graphics.setColor(0, 0, 255) end
+				-- if M[i] then
+				-- love.graphics.polygon('fill',
+				-- surf.vertices[surf.triangles[M[i]][1]].x, surf.vertices[surf.triangles[M[i]][1]].y,
+				-- surf.vertices[surf.triangles[M[i]][2]].x, surf.vertices[surf.triangles[M[i]][2]].y,
+				-- surf.vertices[surf.triangles[M[i]][3]].x, surf.vertices[surf.triangles[M[i]][3]].y
+				-- ) --21/05/16
+				-- end
+			-- end
+		-- end
 	end
-	if #pts ~= 0 then
-		for i = 1, #pts - 1 do
-			love.graphics.line(pts[i].x, pts[i].y, pts[i + 1].x, pts[i + 1].y)
-		end
-		love.graphics.line(pts[#pts].x, pts[#pts].y, pts[1].x, pts[1].y)
-	end
-	-- for i, t in ipairs(triangles) do
-		-- love.graphics.polygon('line', t)
-	-- end
-	-- Vector(400, 300):draw()
-	if A and B then
-		love.graphics.line(A.x, A.y, B.x, B.y)
-	end
-	if TUNNEL then
-		for i, t in ipairs(TUNNEL) do
-			-- love.graphics.setColor(surf.colors[i])
-			-- love.graphics.polygon('fill', t[1].x, t[1].y, t[2].x, t[2].y, t[3].x, t[3].y)
-			surf:removeTriangle(t)
-		end
-		TUNNEL = nil
-		if P then
-			love.graphics.setColor(200, 255, 200, 100)
-			P:draw(10)
-			debug:write(P.class.name..'\n')
-		end
-		if Q then
-			love.graphics.setColor(200, 200, 255, 100)
-			Q:draw(10)
-			debug:write(Q.class.name..'\n')
-		end
+	if A then
+		love.graphics.setColor(255, 255, 255)
+		love.graphics.line(A.x, A.y, love.mouse.getX(), love.mouse.getY())
 	end
 end
 
 function love.mousepressed(x, y, button)
-	if #pts == 0 or pts[#pts]:dist2(x, y) > 25 then
-		table.insert(pts, Vector(x, y))
-	-- if #pts == 0 or pts[#pts]:dist2(cpml.vec2(x, y)) > 25 then
-		-- table.insert(pts, cpml.vec2(x, y))
-	end
+	A = Vector(x, y)
+end
+
+function love.mousereleased(x, y, button)
+	surf:addConstraintEdge(A, Vector(x, y))
+	A = nil
 end
 
 function love.keypressed(key)
-	if key == 'space' then setTriangles() end
-	if key == 'left' then A = Vector(love.mouse.getPosition()) end
-	if key == 'right' then B = Vector(love.mouse.getPosition()) end
-	if key == 't' and A and B then TUNNEL, P, Q = surf:tunnel(A, B) end
-end
-
-function setTriangles()
-	if #pts > 2 then
-		local coords = {}
-		for i, pt in ipairs(pts) do table.insert(coords, pt.x) table.insert(coords, pt.y) end
-		
-		-- triangles = {}
-		-- for i, p in ipairs(Polygon(unpack(coords)):splitConvex()) do
-			-- for j, t in ipairs(p:triangulate()) do
-				-- local tri = {}
-				-- for k, v in ipairs({t:unpack()}) do table.insert(tri, v) end
-				-- table.insert(triangles, tri)
-			-- end
-		-- end
-		surf = Surface(unpack(coords))
-	end
 end
